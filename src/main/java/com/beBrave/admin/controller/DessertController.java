71ef56ea8fc40b2653e269db38806efdc2ee2894
moonzero <82999106+mooonzero@users.noreply.github.com>
2024-11-05 18:49:47 +0900

    getDessertInfoPage 추가

--- src/main/java/com/beBrave/admin/controller/DessertController.java
+++ src/main/java/com/beBrave/admin/controller/DessertController.java
@@ -1,17 +1,28 @@
 package com.beBrave.admin.controller;
 
 import com.beBrave.admin.entity.Dessert;
+import com.beBrave.admin.entity.DessertInfoDto;
+import com.beBrave.admin.service.DessertFacadeService;
 import com.beBrave.admin.service.DessertService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @RestController
-@RequestMapping("/desserts")
+@RequestMapping("/dessert")
 public class DessertController {
 
     @Autowired
     private DessertService dessertService;
+
+    @Autowired
+    private DessertFacadeService dessertFacadeService;
 
     @PostMapping(value = "/add")
     public ResponseEntity<Dessert> addDesserts(@RequestBody Dessert dessert) {
@@ -20,9 +31,15 @@ public class DessertController {
     }
 
     @GetMapping(value = "/get")
-    public Dessert getDesserts(@RequestParam int dessertId) {
+    public Dessert getDessert(@RequestParam int dessertId) {
         return dessertService.getDessertById(dessertId);
 
+    }
+
+    @GetMapping(value = "/list")
+    public Page<DessertInfoDto> getDessertInfoPage(@RequestParam LocalDate date,
+                                                   @PageableDefault(page = 0, size = 10) Pageable pageable) {
+        return dessertFacadeService.getDessertInfoPage(date, pageable);
     }
 
 
--- src/main/java/com/beBrave/admin/repository/DessertRepository.java
+++ src/main/java/com/beBrave/admin/repository/DessertRepository.java
@@ -1,15 +1,20 @@
 package com.beBrave.admin.repository;
 
 import com.beBrave.admin.entity.Dessert;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-@Repository
-public interface DessertRepository extends JpaRepository<Dessert,Integer> {
 
-    List<Dessert> findAllById(Integer id);
+@Repository
+public interface DessertRepository extends JpaRepository<Dessert, Integer> {
+
 
     Dessert findById(int id);
+
     Dessert findByDessertName(String dessertName);
+
+    Page<Dessert> findAll(Pageable pageable);
 }
