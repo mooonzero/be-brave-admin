d1b0b85fa7ad58daa314e530ec71347e85cc36ae
moonzero <82999106+mooonzero@users.noreply.github.com>
2024-11-05 18:52:36 +0900

    미사용 import 제거

--- src/main/java/com/beBrave/admin/controller/DessertController.java
+++ src/main/java/com/beBrave/admin/controller/DessertController.java
@@ -6,7 +6,6 @@ import com.beBrave.admin.service.DessertFacadeService;
 import com.beBrave.admin.service.DessertService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
