743114c6a68fa7d809a3fdb3872bf568e31b4ede
moonzero <82999106+mooonzero@users.noreply.github.com>
2024-11-26 20:09:09 +0900

    [BRAVE-12] 바뀐 프로젝트에 맞춰 service, controller, repo 이름 변경

--- src/main/java/com/beBrave/admin/repository/StockLogRepositoryImpl.java
+++ src/main/java/com/beBrave/admin/repository/StockLogRepositoryImpl.java
@@ -1,14 +1,11 @@
 package com.beBrave.admin.repository;
 
 import com.beBrave.admin.entity.QStockLog;
-import com.beBrave.admin.entity.StockType;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
